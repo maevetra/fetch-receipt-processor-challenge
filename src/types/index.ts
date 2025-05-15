bd9a9e9c931147969c4f3bc10e08329afed7d028
Maeve Tra <maeve.ntra@gmail.com>
2025-05-15 11:21:13 -0400

    feat: add types export

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { Receipt } from '../components/receipt'
+import { Validator } from '../components/validator'
+import { Processor } from '../components/processor'
+
+export { Receipt, Validator, Processor}
